9492e939a67eda65530bf2a390572900951508af
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-07-16 23:46:01 +0200

    feat: Register modules

--- src/main.dart
+++ src/main.dart
@@ -1,8 +1,11 @@
 import 'package:mineral/api.dart';
 import 'package:mineral/core.dart';
 
+import 'guard/guard.dart';
+
 void main () async {
   Kernel kernel = Kernel()
+    ..modules.add(Guard())
     ..intents = [Intent.all];
 
   await kernel.init();
