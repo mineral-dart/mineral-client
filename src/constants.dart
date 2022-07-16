b150bf35cbd2d33f09d24dda875489ce19fee510
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-07-16 23:45:11 +0200

    feat: Create constants

---
+++ src/constants.dart
@@ -0,0 +1,13 @@
+import 'package:mineral/api.dart';
+
+class Roles {
+  static Snowflake member = '988923364921983068';
+}
+
+class Categories {
+  static Snowflake voices = '987752337294635088';
+}
+
+class Channels {
+  static Snowflake createVoiceChannel = '987752264615731260';
+}
